5874d04f974f1ef99a188846a12f6773cd613e9d
pauloreis7 <paulosilvadosreis2057@gmail.com>
2022-05-30 19:29:26 -0300

    add mint to idea details page

--- src/hooks/cache/useGetNFT.ts
+++ src/hooks/cache/useGetNFT.ts
@@ -1,27 +1,27 @@
-import { useQuery } from 'react-query'
+import { useQuery } from "react-query";
 
 // web3
-import { config } from '../../config'
+import { config } from "../../config";
 
 async function fetchUserNFT(account?: string | null): Promise<boolean> {
   if (!account) {
-    return false
+    return false;
   }
 
   try {
-    const contract = config.contracts.BoardIdeas()
-    const [userHasNFT] = await contract.functions.hasAccessPass(account)
+    const contract = config.contracts.BoardIdeas();
+    const [userHasNFT] = await contract.functions.hasAccessPass(account);
 
-    return userHasNFT
+    return userHasNFT;
   } catch (error) {
-    console.log({error})
-    return false
+    console.log({ error });
+
+    return false;
   }
 }
 
 export function useGetNFT(account?: string | null) {
-  return useQuery(['userNFT', account], () => fetchUserNFT(account), {
+  return useQuery(["userNFT", account], () => fetchUserNFT(account), {
     staleTime: 1000 * 60 * 2, // 2 minutes
-
-  })
+  });
 }
